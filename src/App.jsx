c0f71c199fb241113f472243ebc1211307635603
Dawit Gashaw <yepdawit@gmail.com>
2023-07-03 19:44:00 -0400

    added colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 // Write your Color component here
+import React from "react";
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = React.useState(null);
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        {/* colors go here */}
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
